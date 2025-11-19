a14a0863f8508d07c9692a2508f1245f094a07f0
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-19 14:22:09 +0300

    initial commit

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -47,7 +47,7 @@ const AppointmentForm = () => {
     try {
       const hasVisitedBool = Boolean(hasVisited);
       const { data } = await axios.post(
-        "https://mediserve-final-project.onrender.com/api/v1/appointment/post",
+        "http://localhost:4000/api/v1/appointment/post",
         {
           firstName,
           lastName,
