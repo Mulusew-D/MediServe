2a4b78f4d4fc52540f96c0455f03158740234467
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-19 14:20:16 +0300

    initial commit

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -22,7 +22,7 @@ const MessageForm = () => {
 
     try {
       const res = await axios.post(
-        "https://mediserve-final-project.onrender.com/api/v1/message/send",
+        "http://localhost:4000/api/v1/message/send",
         messageData,
         {
           withCredentials: true,
